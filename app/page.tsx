744fb476dc701d90c8871e50dfbbe21cbed0254f
Pruth <pruth@bindu.dev>
2026-09-08 22:50:20 +0530

    Fix Turbopack build error by adding use client to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const HomeHero = dynamic(() => import("@/components/hero/HomeHero").then(mod => ({ default: mod.HomeHero })), { ssr: false });
